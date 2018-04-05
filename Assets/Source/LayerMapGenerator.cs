3aac8f13b231dbe01171770abbeb71862a2d5893
0jb <leveljub@gmail.com>
2018-04-05 07:50:23 -0300

    wip substance to texture2d conversion

---
+++ Assets/Source/LayerMapGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TerrainEngine
+{
+    [ExecuteInEditMode]
+    public class LayerMapGenerator : MonoBehaviour
+    {
+        private ProceduralMaterial _substanceMaterial;
+        private float _parameterValue;
+        private string _parameterName;
+        private string _textureName;
+
+        private void UpdateTexture(float ParameterValue)
+        {
+            _substanceMaterial.SetProceduralFloat(_parameterName, ParameterValue);
+            _substanceMaterial.GetGeneratedTexture(_textureName);
+            //Texture2D newTex;
+            //newTex.SetPixels()
+
+
+        }
+
+    }
+}
+
